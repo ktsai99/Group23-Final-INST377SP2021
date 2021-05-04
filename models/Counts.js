447b630dbfb7890c848d7b5671f797572ae949b7
mhough1 <mhough113@gmail.com>
2021-05-03 23:50:52 -0500

    Implement counts table

---
+++ models/Counts.js
@@ -0,0 +1,26 @@
+export default (database, DataTypes) =>
+{
+    const Counts = database.define(
+        'counts',
+        {
+            catalogue_id:
+            {
+                type: DataTypes.INTEGER,
+                allowNull: false,
+                primaryKey: true
+            },
+            purchase_count:
+            {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            },
+            rental_count:
+            {
+                type: DataTypes.INTEGER,
+                allowNull: false
+            }
+        },
+        { freezeTableName: true, timestamps: false }
+    );
+    return Descriptions;
+};
