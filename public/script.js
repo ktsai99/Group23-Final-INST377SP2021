0a1b2f42648ff0b61b84085896fe5f839a5d6c5d
ktsai99 <78124800+ktsai99@users.noreply.github.com>
2021-05-04 02:56:31 -0400

    updated search bar

--- public/script.js
+++ public/script.js
@@ -262,6 +262,44 @@ async function windowActions()
         list.innerHTML = html;
         
         }
+    //Search bar query
+const endpoint = "/api/movies";
+
+const request = await fetch(endpoint);
+const movies = await request.json();
+
+function findMatches(wordToMatch, movies) {
+    return movies.filter(m => {
+        const regex = new RegExp(wordToMatch, "gi");
+        return m.title.match(regex);
+    });
+}
+function displayMatches(e) {
+    if (event.target.value === "") {
+        suggestions.innerHTML = "";
+        return;
+    }
+    
+const matchArray = findMatches(e.target.value, movies);
+        const html = matchArray.map(m => {
+            return `
+                <li>
+                    <ul>
+                        <li class="address"> ${m.title} — ${m.avg_star_rating} ${m.pricing}</li>
+                    </ul>
+                </li>
+            `;
+        }).join('');
+
+        suggestions.innerHTML = html;
+    }
+    const searchInput = document.querySelector('.search');
+    const form = document.querySelector(".search-form");
+    const suggestions = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", (evt) => { evt.preventDefault(); displayMatches(evt) });
+searchInput.addEventListener("keyup", (evt) => { evt.preventDefault(); displayMatches(evt) });       
+
     let width = 136;
     let count = 1;
     
@@ -302,43 +340,7 @@ async function windowActions()
                 list2.style.marginLeft = position + 'px';
               };
 
-//Search bar query
-const endpoint = "/api/movies";
 
-const request = await fetch(endpoint);
-const movies = await request.json();
-
-function findMatches(wordToMatch, movies) {
-    return movies.filter(m => {
-        const regex = new RegExp(wordToMatch, "gi");
-        return m.title.match(regex);
-    });
-}
-function displayMatches(event) {
-
-const matchArray = findMatches(e.target.value, movies);
-        const html = matchArray.map(m => {
-            return `
-                <li>
-                    <ul>
-                        <li class="address"> ${m.title} — ${m.avg_star_rating} ${m.pricing}</li>
-                    </ul>
-                </li>
-            `;
-        }).join('');
-
-        suggestions.innerHTML = html;
-    }
-    console.log("hello there");
-    const elements = document.querySelector('.suggestions');
-    console.log(elements);
-const form = document.querySelector("link[rel=import][href='search.html']")
-.import.querySelector(".search-form");
-const suggestions = document.querySelector("link[rel=import][href='search.html']")
-.import.querySelector(".suggestions");
-
-searchInput.addEventListener("change", (evt) => { evt.preventDefault(); displayMatches(evt) });
-searchInput.addEventListener("keyup", (evt) => { evt.preventDefault(); displayMatches(evt) });
 
 genres();
 getTVReleaseList();
