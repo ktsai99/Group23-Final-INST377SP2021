1821848494350902f461f0f90b09971540323b91
hchau98 <chauhenry02@gmail.com>
2021-05-06 15:32:43 -0400

    genre commit

---
+++ public/pages/movie-info/script-movie-info.js
@@ -0,0 +1,10 @@
+async function windowActions(){
+    async function populateMovieInfo(movie_id){
+        
+    }
+    document.getElementsByClassName("movie_id").addEventListener((event) =>{
+        const target = event.target;
+        const parent = target.parentElement;
+
+    });
+}
