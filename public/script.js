362fd291dd596f74797d2a5a87ac896f78ed3556
ktsai99 <78124800+ktsai99@users.noreply.github.com>
2021-05-06 15:29:38 -0400

    Update script.js

--- public/script.js
+++ public/script.js
@@ -288,43 +288,6 @@ async function windowActions()
         list.innerHTML = html;
         
         }
-           //Search bar query
-const endpoint = "/api/movies";
-
-const request = await fetch(endpoint);
-const movies = await request.json();
-
-function findMatches(wordToMatch, movies) {
-    return movies.filter(m => {
-        const regex = new RegExp(wordToMatch, "gi");
-        return m.title.match(regex);
-    });
-}
-function displayMatches(e) {
-    if (e.target.value === "") {
-        suggestions.innerHTML = "";
-        return;
-    }
-    
-const matchArray = findMatches(e.target.value, movies);
-        const html = matchArray.map(m => {
-            return `
-                <li>
-                    <ul>
-                        <li class="address"> ${m.title} — ${m.avg_star_rating} ${m.pricing}</li>
-                    </ul>
-                </li>
-            `;
-        }).join('');
-
-        suggestions.innerHTML = html;
-    }
-    const searchInput = document.querySelector('.search');
-    const form = document.querySelector(".search-form");
-    const suggestions = document.querySelector(".suggestions");
-
-searchInput.addEventListener("change", (evt) => { evt.preventDefault(); displayMatches(evt) });
-searchInput.addEventListener("keyup", (evt) => { evt.preventDefault(); displayMatches(evt) });
 
 //Carousel code
     let width = 136;
